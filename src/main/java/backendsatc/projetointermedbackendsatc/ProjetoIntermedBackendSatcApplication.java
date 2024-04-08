4f424331583126a2cfd2d1e96a93552a5b680389
MarchingBeagle <erik.schneider.pacheco@gmail.com>
2024-04-08 19:35:08 -0300

    initial files from spring initializr

---
+++ src/main/java/backendsatc/projetointermedbackendsatc/ProjetoIntermedBackendSatcApplication.java
@@ -0,0 +1,13 @@
+package backendsatc.projetointermedbackendsatc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntermedBackendSatcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntermedBackendSatcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backendsatc/projetointermedbackendsatc/ProjetoIntermedBackendSatcApplicationTests.java
@@ -0,0 +1,13 @@
+package backendsatc.projetointermedbackendsatc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntermedBackendSatcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
